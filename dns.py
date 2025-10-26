a7d31da7a63e0db7db849e715031a4bd47a0edb9
Harinarayan <144616668+HarinarayanJ@users.noreply.github.com>
2025-10-26 18:27:54 +0530

    Init

---
+++ dns.py
@@ -0,0 +1,47 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import OVSSwitch, RemoteController
+from mininet.link import TCLink
+from mininet.cli import CLI
+from mininet.log import setLogLevel
+
+class Topology(Topo):
+    def build(self):
+        # Hosts
+        h1 = self.addHost('h1', ip='10.0.0.1/24')
+        h2 = self.addHost('h2', ip='10.0.0.2/24')
+        h3 = self.addHost('h3', ip='10.0.0.3/24')
+        h4 = self.addHost('h4', ip='10.0.0.4/24')
+        dns = self.addHost('dns', ip='10.0.2.0/24')
+
+        # Switches
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+        s4 = self.addSwitch('s4')
+
+        # Links
+        self.addLink(h1, s1, bw=100, delay='2ms')
+        self.addLink(h2, s2, bw=100, delay='2ms')
+        self.addLink(h3, s3, bw=100, delay='2ms')
+        self.addLink(h4, s4, bw=100, delay='2ms')
+
+        self.addLink(s1, s2, bw=100, delay='5ms')
+        self.addLink(s2, s3, bw=100, delay='8ms')
+        self.addLink(s3, s4, bw=100, delay='10ms')
+        self.addLink(s2, dns, bw=100, delay='1ms')
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    t = Topology()
+    net = Mininet(topo=t, switch=OVSSwitch, link=TCLink)
+    net.start()
+    print("Network is up")
+
+    print("Testing connectivity")
+    net.pingAll()
+
+    CLI(net)
+    net.stop()
+
+
